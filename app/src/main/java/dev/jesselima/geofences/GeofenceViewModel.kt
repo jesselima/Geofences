0bc9dcb994086e5f9394a8d33f68b9069de1daf3
Jesse Lima <jesselima.tech@gmail.com>
2021-05-26 07:42:51 -0300

    Added viewModel structure with SavedStateHandle

--- app/src/main/java/dev/jesselima/geofences/GeofenceViewModel.kt
+++ app/src/main/java/dev/jesselima/geofences/GeofenceViewModel.kt
@@ -1,6 +1,8 @@
 package dev.jesselima.geofences
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 /*
@@ -15,10 +17,44 @@ import androidx.lifecycle.ViewModel
  * the background.
  */
 
-private const val KEY_HINT_INDEX_KEY = "hintIndex"
-private const val KEY_GEOFENCE_INDEX_KEY = "geofenceIndex"
+private const val KEY_HINT_INDEX = "hintIndex"
+private const val KEY_GEOFENCE_INDEX = "geofenceIndex"
 
 class GeofenceViewModel(state: SavedStateHandle) : ViewModel() {
 
+    private val _geofenceIndex = state.getLiveData(KEY_GEOFENCE_INDEX, -1)
+
+    private val _hintIndex = state.getLiveData(KEY_HINT_INDEX, 0)
+
+    val geofenceIndex: LiveData<Int>
+        get() = _geofenceIndex
+
+    val geofenceHintResourceId = Transformations.map(geofenceIndex) {
+        val index = geofenceIndex.value ?: -1
+        when {
+            index < 0 -> R.string.not_started_hint
+            index < GeofencingConstants.getLandMarks().size -> GeofencingConstants.getLandMarks()[geofenceIndex.value!!].hint
+            else -> R.string.geofence_over
+        }
+    }
+
+    val geofenceImageResourceId = Transformations.map(geofenceIndex) {
+        val index = geofenceIndex.value ?: -1
+        when {
+            index < GeofencingConstants.getLandMarks().size -> R.drawable.android_map
+            else -> R.drawable.android_treasure
+        }
+    }
+
+    fun updateHint(currentIndex: Int) {
+        _hintIndex.value = currentIndex+1
+    }
+
+    fun geofenceActivated() {
+        _geofenceIndex.value = _hintIndex.value
+    }
+
+    fun geofenceIsActive() =_geofenceIndex.value == _hintIndex.value
+    fun nextGeofenceIndex() = _hintIndex.value ?: 0
 }
 
